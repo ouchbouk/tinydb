bd93586839e2d458399fe140f58779828dfaf00c
ouchbouk <hakim@themoonhub.com>
2026-05-26 11:11:59 +0100

    add record

---
+++ record.go
@@ -0,0 +1,65 @@
+package tinydb
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+const (
+	headerSize    = 9
+	flagTombstone = byte(1)
+)
+
+type record struct {
+	key   []byte
+	value []byte
+	flags byte
+}
+
+func writeRecord(w io.Writer, r record) error {
+	header := make([]byte, headerSize)
+
+	binary.LittleEndian.PutUint32(header[0:4], uint32(len(r.key)))
+	binary.LittleEndian.PutUint32(header[4:8], uint32(len(r.value)))
+	header[8] = r.flags
+
+	if _, err := w.Write(header); err != nil {
+		return err
+	}
+	if _, err := w.Write(r.key); err != nil {
+		return err
+	}
+	if _, err := w.Write(r.value); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readRecord(r io.Reader) (record, error) {
+	header := make([]byte, headerSize)
+
+	if _, err := io.ReadFull(r, header); err != nil {
+		return record{}, err
+	}
+
+	keyLen := binary.LittleEndian.Uint32(header[0:4])
+	valueLen := binary.LittleEndian.Uint32(header[4:8])
+	flags := header[8]
+
+	key := make([]byte, keyLen)
+	value := make([]byte, valueLen)
+
+	if _, err := io.ReadFull(r, key); err != nil {
+		return record{}, err
+	}
+	if _, err := io.ReadFull(r, value); err != nil {
+		return record{}, err
+	}
+
+	return record{
+		key:   key,
+		value: value,
+		flags: flags,
+	}, nil
+}
